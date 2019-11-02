d4d147c535cca5880b8be29b55652ace2468c9b5
Zach Zemokhol <45083180+SoyStillGood@users.noreply.github.com>
2019-11-02 11:24:27 -0400

    FIXED MAIN INIT

--- myproject.py
+++ myproject.py
@@ -1,48 +1,9 @@
-# import argparse
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import os
-#
-# def load_file():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("-f", "--file", dest="file", help="input data file")
-#
-#     args = parser.parse_args()
-#     file = args.file
-#
-#     diabetes = pd.read_csv(filepath_or_buffer=file, sep=' ', header=0)
-#     print(diabetes)
-#
-# def make_DF():
-#     diabetes = pd.read_csv(filepath_or_buffer=file, sep=' ', header=0)
-#     print(diabetes)
-#     diabetes_df = pd.DataFrame(data=diabetes)
-#
-# def plotting():
-#     plt.plot(diabetes_df['age'])
-#     plt.title('age deviation by index')
-#     plt.xlabel('index')
-#     plt.ylabel('age deviation')
-#     plt.show()
-#
-#
-#
-# def main():
-#     load_file()
-#     make_DF()
-#     plotting()
-#
-#
-# if __name__ == "__main__":
-#     main()
-
-
 import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-def main():
+def load_file():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", dest="file", help="input data file")
 
@@ -51,9 +12,13 @@ def main():
 
     diabetes = pd.read_csv(filepath_or_buffer=file, sep=' ', header=0)
     print(diabetes)
-    diabetes_df = pd.DataFrame(data=diabetes)
 
+def make_DF():
+    diabetes = pd.read_csv(filepath_or_buffer=file, sep=' ', header=0)
+    print(diabetes)
+    return pd.DataFrame(data=diabetes)
 
+def plotting(diabetes_df):
     plt.plot(diabetes_df['age'])
     plt.title('age deviation by index')
     plt.xlabel('index')
@@ -62,8 +27,12 @@ def main():
 
 
 
-
+def main():
+    load_file()
+    diabetes_df = make_DF()
+    plotting(diabetes_df)
 
 
 if __name__ == "__main__":
     main()
+
